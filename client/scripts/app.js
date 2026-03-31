f9e7c73f41e37bac0905b7f08d5f9012ebe07ebb
Panth Patel <panthpsp@gmail.com>
2026-03-31 23:49:48 +0530

    Connected frontend with backend APIs for DH simulation

---
+++ client/scripts/app.js
@@ -0,0 +1,50 @@
+const p_Input = document.getElementById("p");
+const g_Input = document.getElementById("g");
+const a_Input = document.getElementById("alicePrivate");
+const b_Input = document.getElementById("bobPrivate");
+
+const btn_keys = document.getElementById("generateKeys");
+const btn_secret = document.getElementById("generateSecret");
+
+const A_Output = document.getElementById("alicePublic");
+const B_Output = document.getElementById("bobPublic");
+const Secret_Output = document.getElementById("secret");
+
+
+btn_keys.addEventListener("click", async() => {
+    const p = p_Input.value;
+    const g = g_Input.value;
+    const a = a_Input.value;
+    const b = b_Input.value;
+
+    const response = await fetch("http://localhost:5000/api/dh/generate-public", {
+        method : "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({p, g, a, b})
+    });
+
+    const data = await response.json();
+
+    A_Output.textContent = data.A;
+    B_Output.textContent = data.B;
+});
+
+btn_secret.addEventListener('click', async() => {
+    const p = p_Input.value;
+    const a = a_Input.value;
+    const B = B_Output.textContent;
+
+    const response = await fetch("http://localhost:5000/api/dh/generate-secret",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({p, a, B})
+    });
+
+    const data = await response.json();
+
+    Secret_Output.textContent = data.sharedSecret;
+});
